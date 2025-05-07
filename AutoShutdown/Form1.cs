abb4d3c8eb1e7cccd465c34b55b2015e602a657c
Scorp3y <artem.turovetc@gmail.com>
2025-05-07 04:49:22 +0300

    1.0v AutoShutdown

---
+++ AutoShutdown/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AutoShutdown
+{
+    public partial class AutoShutdown : Form
+    {
+
+        [DllImport("powrprof.dll", SetLastError = true)]
+        static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+
+        public AutoShutdown()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int minutes))
+            {
+                int milliseconds = minutes * 60 * 1000;
+                MessageBox.Show($"Компьютер перейдёт в спящий режим через {minutes} мин.");
+                await Task.Delay(milliseconds);
+                SetSuspendState(false, true, true);
+            }
+            
+        }
+    }
+}
